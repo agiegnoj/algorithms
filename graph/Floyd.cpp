eda9783e19814b6dad8416e1e53bc972053d9763
Jon Geiger Gajate <jongg97@gmail.com>
2025-03-03 22:49:28 +0100

    .

--- graph/Floyd.cpp
+++ graph/Floyd.cpp
@@ -1,14 +1,56 @@
 #include <vector>
 using namespace std;
+template <typename T>
 
 class Floyd{
     public: 
+
+
+
+    bool hasCycle(vector<pair<int,T>>& successorGraph, int start){ //<pointer, data>
+        int slow = start;
+        int fast = start;
+
+        while (fast != -1 && successorGraph[fast].first != -1 ){
+            slow = successorGraph[slow].first;
+            fast = successorGraph[successorGraph[fast].first].first;
+
+            if (slow == fast) return true;
+
+        }
+
+        return false;
+
+
+    }
+
+    pair<int, T> getCycleBeginning(vector<pair<int,T>>& successorGraph, int start){
+        int slow = start;
+        int fast = start;
+
+        while (fast != -1 && successorGraph[fast].first != -1){
+            slow = successorGraph[slow].first;
+            fast = successorGraph[successorGraph[fast].first].first;
+
+            if (slow == fast){
+                fast = start;
+                while (fast != slow){
+                    fast = successorGraph[fast].first;
+                    slow = successorGraph[slow].first;
+                }
+
+                return successorGraph[slow];
+            } 
+        }
+
+        return {-1, T()};
+    }
 
     bool hasCycle(vector<int>& successorGraph, int start){
         int slow = start;
         int fast = start;
 
-        while (fast != -1 && successorGraph[fast] != -1 && successorGraph[successorGraph[fast]] != -1){
+        while (fast != -1 && successorGraph[fast] != -1 ){
             slow = successorGraph[slow];
             fast = successorGraph[successorGraph[fast]];
 
@@ -25,7 +67,7 @@ class Floyd{
         int slow = start;
         int fast = start;
 
-        while (fast != -1 && successorGraph[fast] != -1 && successorGraph[successorGraph[fast]] != -1){
+        while (fast != -1 && successorGraph[fast] != -1 ){
             slow = successorGraph[slow];
             fast = successorGraph[successorGraph[fast]];
 
