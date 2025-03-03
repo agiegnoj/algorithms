29b725411d0d3490906111fa1ac0ea82ff786897
Jon Geiger Gajate <jongg97@gmail.com>
2025-03-03 18:45:51 +0100

    .

---
+++ dynamic_programming/Floyd.cpp
@@ -0,0 +1,45 @@
+#include <vector>
+using namespace std;
+
+class Floyd{
+    public: 
+
+    bool hasCycle(vector<int>& successorGraph, int start){
+        int slow = start;
+        int fast = start;
+
+        while (fast != -1 && successorGraph[fast] != -1 && successorGraph[successorGraph[fast]] != -1){
+            slow = successorGraph[slow];
+            fast = successorGraph[successorGraph[fast]];
+
+            if (slow == fast) return true;
+
+        }
+
+        return false;
+
+
+    }
+
+    int getCycleBeginning(vector<int>& successorGraph, int start){
+        int slow = start;
+        int fast = start;
+
+        while (fast != -1 && successorGraph[fast] != -1 && successorGraph[successorGraph[fast]] != -1){
+            slow = successorGraph[slow];
+            fast = successorGraph[successorGraph[fast]];
+
+            if (slow == fast){
+                fast = start;
+                while (fast != slow){
+                    fast = successorGraph[fast];
+                    slow = successorGraph[slow];
+                }
+
+                return slow;
+            } 
+        }
+
+        return -1;
+    }
+};
