e0f630d1a64641044f09c3a5f7871eb607c97db6
Jon Geiger Gajate <jongg97@gmail.com>
2025-03-03 23:10:28 +0100

    .

--- graph/Floyd.cpp
+++ graph/Floyd.cpp
@@ -5,45 +5,6 @@ template <typename T>
 class Floyd{
     public: 
 
-    bool hasCycle(vector<pair<int,T>>& successorGraph, int start){ //<pointer, data>
-        int slow = start;
-        int fast = start;
-
-        while (fast != -1 && successorGraph[fast].first != -1 ){
-            slow = successorGraph[slow].first;
-            fast = successorGraph[successorGraph[fast].first].first;
-
-            if (slow == fast) return true;
-
-        }
-
-        return false;
-
-
-    }
-
-    pair<int, T> getCycleBeginning(vector<pair<int,T>>& successorGraph, int start){
-        int slow = start;
-        int fast = start;
-
-        while (fast != -1 && successorGraph[fast].first != -1){
-            slow = successorGraph[slow].first;
-            fast = successorGraph[successorGraph[fast].first].first;
-
-            if (slow == fast){
-                fast = start;
-                while (fast != slow){
-                    fast = successorGraph[fast].first;
-                    slow = successorGraph[slow].first;
-                }
-
-                return successorGraph[slow];
-            } 
-        }
-
-        return {-1, T()};
-    }
-
     bool hasCycle(vector<int>& successorGraph, int start){
         int slow = start;
         int fast = start;
